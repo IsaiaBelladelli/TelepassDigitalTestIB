2093ae17cf8b0bc9aeb42c87ddc2a818eebb72ff
Isaia Belladelli <isaia.belladelli@gmail.com>
2021-11-11 08:50:58 +0100

    Initial Commit

---
+++ TelepassDigitalTestIB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TelepassDigitalTestIB
+//
+//  Created by Isaia Belladelli on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TelepassDigitalTestIB/TelepassDigitalTestIBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelepassDigitalTestIBApp.swift
+//  TelepassDigitalTestIB
+//
+//  Created by Isaia Belladelli on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TelepassDigitalTestIBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
